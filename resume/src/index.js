4fe59e0a8fd9a95f6f22f6711f14d4c2bdfffc96
rustleupchef <rustle.up.chef@gmail.com>
2026-05-12 14:40:34 -0500

    added md page to the site

--- resume/src/index.js
+++ resume/src/index.js
@@ -2,15 +2,29 @@ import { marked } from "marked";
 
 export default {
 	async fetch(request, env, ctx) {
-		
-		const response = await fetch("https://raw.githubusercontent.com/rustleupchef/Job/master/resume.md");
-		const markdown = await response.text();
 
-		const html = marked(markdown);
-		return new Response(html, {
-			headers: {
-				"Content-Type": "text/html",
-			},
-		});
-	},
+		if (request.method === "GET" && new URL(request.url).pathname === "/") {
+		
+			const response = await fetch("https://raw.githubusercontent.com/rustleupchef/Job/master/resume.md");
+			const markdown = await response.text();
+
+			const html = marked(markdown);
+			return new Response(html, {
+				headers: {
+					"Content-Type": "text/html",
+				},
+			});
+		}  else if (request.method === "GET" && new URL(request.url).pathname === "/resume.md") {
+			const response = await fetch("https://raw.githubusercontent.com/rustleupchef/Job/master/resume.md");
+			const markdown = await response.text();
+
+			return new Response(markdown, {
+				headers: {
+					"Content-Type": "text/markdown",
+				},
+			});
+		} else {
+			return new Response("Not Found", { status: 404 });
+		}
+	}
 };
